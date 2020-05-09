1347bb71350ee4ffa6ef4d36f01b486e58648cf4
Foo <twistleech@gmail.com>
2020-05-09 19:14:25 +0200

    Add files via upload

---
+++ rewriteRules.php
@@ -0,0 +1,9 @@
+<?php
+
+// Available placeholders: ipinfo, laravelipinfo, ipinfo, laravelipinfo
+return [
+    'src/MyPackage.php' => 'src/laravelipinfo.php',
+    'config/mypackage.php' => 'config/laravelipinfo.php',
+    'src/Facades/MyPackage.php' => 'src/Facades/laravelipinfo.php',
+    'src/MyPackageServiceProvider.php' => 'src/ipinfolaravelServiceProvider.php',
+];
